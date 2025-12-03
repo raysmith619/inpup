a9ea72fbea88f5dff52f3b75de3595f2514ce207
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2025-12-03 15:34:53 -0500

    Update presentation, .toml

---
+++ ip_files/presentation/Class_1_Introduction/samples/square_loop_colors_80.py
@@ -0,0 +1,12 @@
+# square_loop_colors.py
+# Display a square
+
+import turtle as tu     # Coloring tools
+
+colors = ["red", "orange", "yellow", "green"]
+tu.width(80)
+for color in colors:  # Do for all colors
+    tu.color(color)
+    tu.forward(200)
+    tu.right(90)
+tu.done()
